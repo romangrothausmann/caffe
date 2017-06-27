535a73fbafa2d2b8f0076470ade31bf7fe8154ff
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2017-06-27 09:15:30 +0200

    Revert "only load H5 once (iter: 0)"
    This reverts commit 9b41f3fb1ae38505bc897418cfaeac3b0c35e28e.

--- src/caffe/layers/hdf5_data_layer.cpp
+++ src/caffe/layers/hdf5_data_layer.cpp
@@ -183,11 +183,13 @@ void HDF5DataLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
 template <typename Dtype>
 void HDF5DataLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-  if( current_row_ == 0) {
-      int i = 0;
+  const int batch_size = this->layer_param_.hdf5_data_param().batch_size();
+  for (int i = 0; i < batch_size; ++i) {
     // if at the begin of a new file, load the data
+    if( current_row_ == 0) {
       LoadHDF5FileData(
           hdf_filenames_[file_permutation_[current_file_]].c_str());
+    }
 
     // copy data to top blobs
     for (int j = 0; j < this->layer_param_.top_size(); ++j) {
@@ -197,7 +199,24 @@ void HDF5DataLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
             * data_dim], &top[j]->mutable_cpu_data()[i * data_dim]);
     }
 
+    // advance index to next "row", possibly go to next file
     ++current_row_;
+    if (current_row_ == hdf_blobs_[0]->shape(0)) {
+      if (num_files_ > 1) {
+        ++current_file_;
+        if (current_file_ == num_files_) {
+          current_file_ = 0;
+          if (this->layer_param_.hdf5_data_param().shuffle()) {
+            std::random_shuffle(file_permutation_.begin(),
+                                file_permutation_.end());
+          DLOG(INFO) << "Looping around to first file.";
+          }
+        }
+      }
+      current_row_ = 0;
+      if (this->layer_param_.hdf5_data_param().shuffle())
+        std::random_shuffle(data_permutation_.begin(), data_permutation_.end());
+    }
   }
 }
 
